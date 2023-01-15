d34c7345d4815e399257cafeb8e350f8e8c55995
Sathishkumar Perumal <sathishkumar.perumal@logicinfo.com>
2023-01-15 17:16:00 +0530

    Updates

---
+++ precache-manifest.72ef0cf0e03d39ae1da4ed4272bc2eb7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "684c758533c6f8616285",
+    "url": "/TestVijayRepo/static/css/main.d008f143.chunk.css"
+  },
+  {
+    "revision": "684c758533c6f8616285",
+    "url": "/TestVijayRepo/static/js/main.684c7585.chunk.js"
+  },
+  {
+    "revision": "205606f78eb923af0e8b",
+    "url": "/TestVijayRepo/static/css/1.d866bdad.chunk.css"
+  },
+  {
+    "revision": "205606f78eb923af0e8b",
+    "url": "/TestVijayRepo/static/js/1.205606f7.chunk.js"
+  },
+  {
+    "revision": "21f338438936a37e3c19",
+    "url": "/TestVijayRepo/static/js/runtime~main.21f33843.js"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/TestVijayRepo/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/TestVijayRepo/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/TestVijayRepo/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/TestVijayRepo/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/TestVijayRepo/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/TestVijayRepo/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/TestVijayRepo/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/TestVijayRepo/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/TestVijayRepo/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/TestVijayRepo/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/TestVijayRepo/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/TestVijayRepo/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/TestVijayRepo/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/TestVijayRepo/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/TestVijayRepo/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/TestVijayRepo/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "b71172af8d3efb2fca92e20139874755",
+    "url": "/TestVijayRepo/index.html"
+  }
+];
